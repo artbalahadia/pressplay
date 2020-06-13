95a9bac8e7566605b5ce74bf99105a4ba1cf3f75
Art Balahadia <63167845+artbalahadia@users.noreply.github.com>
2020-06-13 17:23:27 -0500

    Updates

---
+++ precache-manifest.7e6c94b3eafc2a9974b71b8a3353cf57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87407fc7d3d6f82d6cc9ae53c70d1e0c",
+    "url": "/pressplay/index.html"
+  },
+  {
+    "revision": "22e15478ac95cfeaf330",
+    "url": "/pressplay/static/css/main.d3628fda.chunk.css"
+  },
+  {
+    "revision": "f1ff948474ac0c31c017",
+    "url": "/pressplay/static/js/2.91eba243.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/pressplay/static/js/2.91eba243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e15478ac95cfeaf330",
+    "url": "/pressplay/static/js/main.f07663c3.chunk.js"
+  },
+  {
+    "revision": "9b0f9a956d53f8a46d1e",
+    "url": "/pressplay/static/js/runtime-main.dc936f4e.js"
+  }
+]);
